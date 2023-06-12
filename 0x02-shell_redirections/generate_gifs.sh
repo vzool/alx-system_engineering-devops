828b9e58506bcb7b0151f110779111a271c6784f
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2023-06-12 14:26:57 +0300

    Add files and directories structure generator for the test

---
+++ 0x02-shell_redirections/generate_gifs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf test_dir
+mkdir -p test_dir/photos
+mkdir test_dir/rep.gif
+touch test_dir/docker.gif
+touch test_dir/file.sh
+touch test_dir/README.md
+touch test_dir/.horrible_selfie.gif
+touch test_dir/photos/cat.gif
+touch test_dir/photos/Electra_napping.gif
+touch test_dir/photos/main.gif
+touch test_dir/photos/index.html
